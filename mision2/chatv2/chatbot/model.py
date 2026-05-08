ed7147a7b3ec4378c715ad5b5e64eeba55057994
Mario Montoya <mario.montoyao@jjyciao.com>
2026-05-07 22:37:15 -0500

    Agregar mision2

---
+++ mision2/chatv2/chatbot/model.py
@@ -0,0 +1,7 @@
+import os # Sistema Operativo (archivos y rutas)
+import pickle
+
+from sklearn.feature_extraction.text import CountVectorizer
+# CountVectorizer convierte texto en un vector
+from sklearn.naive_bayes import MultinomialNB
+
